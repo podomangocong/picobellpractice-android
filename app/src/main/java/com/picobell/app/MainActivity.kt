003e7ff5a52690870f60d5444e03d3f763fd320b
ijimin <zhzhwlals12@gmail.com>
2022-07-28 20:18:40 +0900

    [섹션5] Fragment 추가 및 화면 연결

--- app/src/main/java/com/picobell/app/MainActivity.kt
+++ app/src/main/java/com/picobell/app/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.picobell.app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
     }
 }
